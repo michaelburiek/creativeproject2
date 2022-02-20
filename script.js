daa783edef156f7945f18543b1f8a78dd8aa14cd
michaelburiek <michaelburiek0@gmail.com>
2022-02-20 09:29:24 -0800

    some fixes

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 document.getElementById("cryptoSearchSubmit").addEventListener("click", function(event) {
-  event.prevendivefault();
+  event.preventDefault();
   const cryptoTicker = document.getElementById("cryptoTickerInput").value;
+  console.log(cryptoTicker);
   const multiplier = document.getElementById("multiplierInput").value;
+  console.log(multiplier);
   const timespan = document.getElementById("timespanInput").value;
+  console.log(timespan);
   const from = document.getElementById("fromInput").value;
+  console.log(from);
   const to = document.getElementById("toInput").value;
+  console.log(to);
   //if (value === "")
-  //  return;
+    //return;
 
-  // let url = "https://api.polygon.io/v2/aggs/ticker/cryptoTicker/range/multiplier/timespan/from/to?adjusted=true&sort=asc&limit=120&apiKey=fSFWGZnAAX_9ROoxelYbVqpNzpARpsiT";
-  let url = "https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/2022-02-18/2022-02-18?adjusted=true&sort=asc&limit=120&apiKey=fSFWGZnAAX_9ROoxelYbVqpNzpARpsiT";
-  fetch(url).diven(response => response.json())
-  .diven(function(json) {
+  //let url = "https://api.polygon.io/v2/aggs/ticker/cryptoTicker/range/multiplier/timespan/from/to?adjusted=true&sort=asc&limit=120&apiKey=fSFWGZnAAX_9ROoxelYbVqpNzpARpsiT";
+  let url = "https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/2021-02-18/2021-02-18?adjusted=true&sort=asc&limit=120&apiKey=fSFWGZnAAX_9ROoxelYbVqpNzpARpsiT";
+  fetch(url).then(response => response.json())
+  .then(function(json) {
     let results = " ";
     results =
     `
-      <div class="crypto-results-container">
-          <div class="crypto-results-item">
+      <div>
+          <div>
             <div>OPEN</div>
             <div>$${new Intl.NumberFormat().format(json.results[0].o)}</div>
           </div>
 
-          <div class="crypto-results-item">
+          <div>
             <div>CLOSE</div>
             <div>$${new Intl.NumberFormat().format(json.results[0].c)}
           </div>
 
-          <div class="crypto-results-item">
+          <div>
             <div>LOWEST</div>
             <div>$${new Intl.NumberFormat().format(json.results[0].l)}</div>
           </div>
 
-          <div class="crypto-results-item">
+          <div>
             <div>HIGHEST</div>
             <div>$${new Intl.NumberFormat().format(json.results[0].h)}</div>
           </div>
 
-          <div class="crypto-results-item">
+          <div>
             <div>VOLUME</div>
             <div>${new Intl.NumberFormat().format(json.results[0].v)}</div>
           </div>
